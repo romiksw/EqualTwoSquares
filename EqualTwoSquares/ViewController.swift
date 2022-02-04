4051c40564960afa20b2720a6d7a815d1bdd8dcb
Roman <roman.d.kozlov@gmail.com>
2022-02-04 20:54:23 +0300

    Initial Commit

---
+++ EqualTwoSquares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EqualTwoSquares
+//
+//  Created by Roman Kozlov on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
